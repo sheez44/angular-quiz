48b8b5ba11f88e933ad373e2052a882ed67ff381
sheez <blaat2k@mail.com>
2015-04-02 19:57:09 +0200

    update set function, upload userInfo logic

--- builds/development/js/services/authFactory.js
+++ builds/development/js/services/authFactory.js
@@ -4,7 +4,7 @@
 		.factory("Auth", ["$firebaseAuth", 
 			"$firebase", "$location", "$routeParams", "CONSTANTS", function($firebaseAuth, 
 				$firebase, routeParams, $location, CONSTANTS) {
-			
+
 			var ref = new Firebase(CONSTANTS.FIREBASE_URL);
 			var auth = $firebaseAuth(ref);
 
@@ -20,7 +20,29 @@
 					return auth.$createUser({
 						email: user.email,
 						password: user.password
-					});
+					}).then(function(regUser) { // When the data is sent to firebase, an object 'regUser' is returned 
+						var ref = new Firebase(CONSTANTS.FIREBASE_URL + "users/" + regUser.uid);
+
+					var userInfo = {
+							date: Firebase.ServerValue.TIMESTAMP,
+							regUser: regUser.uid,
+							name: user.name,
+							username: user.username,
+							email: user.email
+						}; // User Info object
+
+						console.log(userInfo);
+
+						ref.set(userInfo, function(error) {
+							if(error) {
+								console.log("Error: ", error);
+							} else {
+								console.log("created profile successfully")
+							}
+						});
+
+						
+					}); 
 				} // register
 
 			}; // myObject
